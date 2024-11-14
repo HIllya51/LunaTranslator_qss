cea72773a92eb79da4325ebb82054326c5ad8f47
恍兮惚兮 <1173718158@qq.com>
2024-11-15 01:21:17 +0800

    prepare

---
+++ PyQtDarkTheme/light.py
@@ -0,0 +1,84 @@
+import os
+import sys
+import json
+
+sys.path.append(os.path.dirname(__file__))
+import qdarktheme
+from gui.inputdialog import autoinitdialog
+
+theme = "light"
+
+
+def tryloadconfig():
+    try:
+        with open(__file__ + ".json", "r", encoding="utf8") as ff:
+            return json.loads(ff.read())
+    except:
+        return {}
+
+
+ACCENT_COLORS = {
+    "dark": {
+        "blue": "#8ab4f7",
+        "graphite": "#898a8f",
+        "green": "#4caf50",
+        "orange": "#ff9800",
+        "pink": "#c7457f",
+        "purple": "#af52bf",
+        "red": "#f6685e",
+        "yellow": "#ffeb3b",
+    },
+    "light": {
+        "blue": "#1a73e8",
+        "graphite": "#898a8f",
+        "green": "#4caf50",
+        "orange": "#ff9800",
+        "pink": "#c7457f",
+        "purple": "#9c27b0",
+        "red": "#f44336",
+        "yellow": "#f4c65f",
+    },
+}
+
+
+def get_setting_window(parent, callback):
+
+    config = tryloadconfig()
+
+    def callback1():
+        with open(__file__ + ".json", "w", encoding="utf8") as ff:
+            ff.write(json.dumps(config))
+        callback()
+
+    autoinitdialog(
+        parent,
+        config,
+        "PyQtDarkTheme",
+        600,
+        [
+            {
+                "type": "combo",
+                "name": "corner shape",
+                "k": "corner_shape",
+                "list": ["rounded", "sharp"],
+            },
+            {
+                "type": "combo",
+                "name": "color",
+                "k": "color",
+                "list": list(ACCENT_COLORS[theme].keys()),
+            },
+            {"type": "okcancel", "callback": callback1},
+        ],
+    )
+
+
+def stylesheet():
+    config = tryloadconfig()
+    corner_shape = config.get("corner_shape", 0)
+    color = config.get("color", 0)
+    return qdarktheme.load_stylesheet(
+        theme=theme,
+        corner_shape=["rounded", "sharp"][corner_shape],
+        custom_colors={"primary": list(ACCENT_COLORS[theme].values())[color]},
+    )
---
+++ qt_material/light.py
@@ -0,0 +1,85 @@
+import os, sys, json
+
+sys.path.append(os.path.dirname(__file__))
+from qtsymbols import *
+from gui.inputdialog import autoinitdialog
+from qt_material import build_stylesheet
+
+
+def tryloadconfig():
+    try:
+        with open(__file__ + ".json", "r", encoding="utf8") as ff:
+            return json.loads(ff.read())
+    except:
+        return {}
+
+
+def get_setting_window(parent, callback):
+
+    config = tryloadconfig()
+
+    def callback1():
+        with open(__file__ + ".json", "w", encoding="utf8") as ff:
+            ff.write(json.dumps(config))
+        callback()
+
+    autoinitdialog(
+        parent,
+        config,
+        "qt material",
+        600,
+        [
+            {
+                "type": "combo",
+                "name": "Theme name",
+                "k": "theme",
+                "list": [
+                    "amber",
+                    "blue",
+                    "blue_500",
+                    "cyan",
+                    "cyan_500",
+                    "lightgreen",
+                    "lightgreen_500",
+                    "orange",
+                    "pink",
+                    "pink_500",
+                    "purple",
+                    "purple_500",
+                    "red",
+                    "red_500",
+                    "teal",
+                    "teal_500",
+                    "yellow",
+                ],
+            },
+            {"type": "okcancel", "callback": callback1},
+        ],
+    )
+
+
+def stylesheet():
+    config = tryloadconfig().get("theme", 0)
+    return build_stylesheet(
+        [
+            "light_amber.xml",
+            "light_blue.xml",
+            "light_blue_500.xml",
+            "light_cyan.xml",
+            "light_cyan_500.xml",
+            "light_lightgreen.xml",
+            "light_lightgreen_500.xml",
+            "light_orange.xml",
+            "light_pink.xml",
+            "light_pink_500.xml",
+            "light_purple.xml",
+            "light_purple_500.xml",
+            "light_red.xml",
+            "light_red_500.xml",
+            "light_teal.xml",
+            "light_teal_500.xml",
+            "light_yellow.xml",
+        ][config],
+        False,
+        {},
+    )
