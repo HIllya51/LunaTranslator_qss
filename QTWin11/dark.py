0ccbf59b08fd7458820ede53833a8968e93b0d0c
恍兮惚兮 <1173718158@qq.com>
2025-01-09 23:00:55 +0800

    .

--- QTWin11/dark.py
+++ QTWin11/dark.py
@@ -1,20 +1,14 @@
 from winreg import *
 from . import styledark_rc
-from myutils.config import get_platform
-
 
 def stylesheet():
-    if get_platform() == "xp":
-        return ""
-    try:
-        registry = ConnectRegistry(None, HKEY_CURRENT_USER)
-        key = OpenKey(
-            registry, r"SOFTWARE\\Microsoft\Windows\\CurrentVersion\\Explorer\\Accent"
-        )
-        key_value = QueryValueEx(key, "AccentColorMenu")
-        accent_int = key_value[0]
-    except:
-        accent_int = 0xFFD47800
+    
+    registry = ConnectRegistry(None, HKEY_CURRENT_USER)
+    key = OpenKey(
+        registry, r"SOFTWARE\\Microsoft\Windows\\CurrentVersion\\Explorer\\Accent"
+    )
+    key_value = QueryValueEx(key, "AccentColorMenu")
+    accent_int = key_value[0] 
     accent = accent_int - 4278190080
     accent = str(hex(accent)).split("x")[1]
     accent = accent[4:6] + accent[2:4] + accent[0:2]
